f1ef2d08a31ac55ca9c5ca8b82cc716c3c6a8895
Muhammad Irfan Raza <52914975+irfaanraza@users.noreply.github.com>
2021-09-28 20:10:36 +0500

    Add files via upload
    Task

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Post from './components/Post';
+import Get from './components/Get';
+import Delete from './components/Delete';
+import Put from './components/Put';
+
+function App() {
+  return (
+    <div className='App'>
+      <Get />
+    </div>
+  );
+}
+
+export default App;
